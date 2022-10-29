63d9223a5afa37b8e889c01462df57547ae2c3fb
Ricky Tran <diabloxtran@gmail.com>
2022-10-29 16:35:20 -0400

    added user and faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,1 @@
 require 'faker'
-
-50.times do
-  User.create!(
-    email: Faker::Internet.email,
-    encrypted_password: Faker::Alphanumeric.alphanumeric(number: 15, min_alpha: 7, min_numeric: 3)
-  )
-end
