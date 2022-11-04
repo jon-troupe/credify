d25351d1ca3a890d486e503cd542cebd93a38705
Jonathan Troupe <sanetroupe@gmail.com>
2022-11-03 20:35:41 -0400

    Removed validations from cc, link to create a reservation from show cc page and ability to see all reservations there too

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -11,13 +11,10 @@ class CreditCard < ApplicationRecord
   validates :price_per_day, presence: true
   validates :cardholder, presence: true
   validates :number, length: { is: 8 }
-  validates :pin, length: { is: 4 }
   validates :card_type, inclusion: { in: ["Costco", "Triangle", "VISA", "Mastercard", "AMEX"],
     message: "%{value} is not a card type" }
   validates :card_issuer, inclusion: { in: ["BMO", "Scotiabank", "Bank of America", "Swiss Credit Union", "TD", "Desjardins"],
     message: "%{value} is not a card issuer" }
   validates :credit_limit, length: { in: 3..10 }
   validates :price_per_day, length: { in: 1..5 }
-  validates :date, format: { with: /((0[1-9])|(1[0-2]))\/(2[2-9])/,
-    message: "Please respect MM/YY format" }
 end
