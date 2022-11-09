748c9ff53bb1b5f1e0565bcc98eab042fc97bb01
Jonathan Troupe <sanetroupe@gmail.com>
2022-11-08 19:42:13 -0500

    Added four static users to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,30 @@
 require 'faker'
 
+Reservation.destroy_all
 CreditCard.destroy_all
+User.destroy_all
 
-100.times do
+ricky = User.create!(
+  email: "ricky.tran@gmail.com",
+  password: "123456"
+)
+
+laura = User.create!(
+  email: "laura.gonzalez@gmail.com",
+  password: "123456"
+)
+
+touhami = User.create!(
+  email: "touhami.abi@gmail.com",
+  password: "123456"
+)
+
+jon = User.create!(
+  email: "jonathan.troupe@gmail.com",
+  password: "123456"
+)
+
+33.times do
   CreditCard.create!(
     cardholder: Faker::Name.name,
     number:Faker::Bank.account_number(digits: 8),
@@ -13,6 +35,36 @@ CreditCard.destroy_all
     date: "#{(1..12).to_a.sample}/#{(23..28).to_a.sample}",
     address: Faker::Address.street_address,
     price_per_day: (50..200).to_a.sample,
-    user_id: 1
+    user: laura
+  )
+end
+
+33.times do
+  CreditCard.create!(
+    cardholder: Faker::Name.name,
+    number:Faker::Bank.account_number(digits: 8),
+    pin: Faker::Bank.account_number(digits: 4),
+    card_type: CreditCard::CARD_TYPE.sample,
+    card_issuer: CreditCard::ISSUERS.sample,
+    credit_limit: Faker::Number.number(digits: 4),
+    date: "#{(1..12).to_a.sample}/#{(23..28).to_a.sample}",
+    address: Faker::Address.street_address,
+    price_per_day: (50..200).to_a.sample,
+    user: touhami
+  )
+end
+
+33.times do
+  CreditCard.create!(
+    cardholder: Faker::Name.name,
+    number:Faker::Bank.account_number(digits: 8),
+    pin: Faker::Bank.account_number(digits: 4),
+    card_type: CreditCard::CARD_TYPE.sample,
+    card_issuer: CreditCard::ISSUERS.sample,
+    credit_limit: Faker::Number.number(digits: 4),
+    date: "#{(1..12).to_a.sample}/#{(23..28).to_a.sample}",
+    address: Faker::Address.street_address,
+    price_per_day: (50..200).to_a.sample,
+    user: jon
   )
 end
