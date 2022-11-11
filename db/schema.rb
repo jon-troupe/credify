7f53c2a2f00324733793c5aa39a69c0f8cd51bba
touhami abi <abitouhami26@gmail.com>
2022-11-10 19:54:00 -0500

    add dotenv-rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_29_201449) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_000034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_201449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cardholder"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
